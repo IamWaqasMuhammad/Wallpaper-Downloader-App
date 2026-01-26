c6d9264dfeb34929f2d398e460e6b21862b37649
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2026-01-26 18:31:48 +0500

    first commit

---
+++ lib/app_barrels.dart
@@ -0,0 +1,25 @@
+// Flutter & Material
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:file_saver/file_saver.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:flutter_animate/flutter_animate.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+export 'package:gal/gal.dart';
+// Third Party Packages
+export 'package:get/get.dart';
+export 'package:google_fonts/google_fonts.dart';
+export 'package:lottie/lottie.dart';
+export 'package:path_provider/path_provider.dart';
+export 'package:permission_handler/permission_handler.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:wallpaper_downloader/core/bindings/initial_binding.dart';
+// Core Services & Theme
+export 'package:wallpaper_downloader/core/services/theme_service.dart';
+export 'package:wallpaper_downloader/core/theme/app_theme.dart';
+// Routes
+export 'package:wallpaper_downloader/routes/app_pages.dart';
+export 'package:wallpaper_downloader/routes/app_routes.dart';
+
+// You can add more exports here as needed
